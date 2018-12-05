52019012eb6cd14d51a2b5c7dec2a1a10cf38a46
Toren Wallengren <toren.wallengren@addepar.com>
2018-12-04 19:37:18 -0700

    made navbar component

---
+++ src/features/navbar/index.js
@@ -0,0 +1,53 @@
+/*
+
+Usage
+<NavBar />
+
+Main NavBar.js component - contains game world
+
+*/
+
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+
+            <div
+                style={{
+                    color: 'white',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                <Link to="/">
+                    <div className='links'>MAIN MENU</div>
+                </Link>
+                <Link to="/lemonscates">
+                    <div className='links'>CENTRAL CITY</div>
+                </Link>
+                <Link to="/forest">
+                    <div className='links'>FOREST WORLD</div>
+                </Link>
+                <Link to="/desert">
+                    <div className='links'>DESERT WORLD</div>
+                </Link>
+                <Link to="/beach">
+                    <div className='links'>BEACH WORLD</div>
+                </Link>
+                <Link to="/winter">
+                    <div className='links'>WINTER WORLD</div>
+                </Link>
+            </div>
+
+        );
+    }
+}
+
+export default NavBar;
